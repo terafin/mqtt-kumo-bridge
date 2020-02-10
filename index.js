2cd8c1eefd795421c0b5ef4976a78cf42fe25837
Justin Wood <jwood@me.com>
2020-02-10 14:44:31 -0800

    Updating Kumo Config

--- index.js
+++ index.js
@@ -19,112 +19,118 @@ const password = process.env.KUMO_PASS
 
 
 if (_.isNil(username)) {
-	logging.warn('KUMO_USER not set, not starting')
-	process.abort()
+    logging.warn('KUMO_USER not set, not starting')
+    process.abort()
 }
 
 if (_.isNil(password)) {
-	logging.warn('KUMO_PASS not set, not starting')
-	process.abort()
+    logging.warn('KUMO_PASS not set, not starting')
+    process.abort()
 }
 
 if (_.isNil(topic_prefix)) {
-	logging.warn('TOPIC_PREFIX not set, not starting')
-	process.abort()
+    logging.warn('TOPIC_PREFIX not set, not starting')
+    process.abort()
 }
 
-var mqttOptions = {qos: 1, retain: true}
+var mqttOptions = { qos: 1, retain: true }
 
 var shouldRetain = process.env.MQTT_RETAIN
 
 if (_.isNil(shouldRetain)) {
-	shouldRetain = true
+    shouldRetain = true
 }
 
 mqttOptions['retain'] = shouldRetain
 
 var connectedEvent = function() {
-	health.healthyEvent()
+    health.healthyEvent()
 
-	const topics = [topic_prefix + '/+/+/set']
+    const topics = [topic_prefix + '/+/+/set']
 
-	logging.info('Connected, subscribing ')
-	topics.forEach(function(topic) {
-		logging.info(' => Subscribing to: ' + topic)
-		client.subscribe(topic, {qos: 1})
-	}, this)
+    logging.info('Connected, subscribing ')
+    topics.forEach(function(topic) {
+        logging.info(' => Subscribing to: ' + topic)
+        client.subscribe(topic, { qos: 1 })
+    }, this)
 }
 
 var disconnectedEvent = function() {
-	health.unhealthyEvent()
+    health.unhealthyEvent()
 }
 
 const client = mqtt_helpers.setupClient(connectedEvent, disconnectedEvent)
 
 
-config.auth(username, password, function(config){
-	logging.info('Done auth: ' + JSON.stringify(config))
-    
-	kumo = new kumoLib(config)
+config.auth(username, password, function(config) {
+    logging.info('Done auth: ' + JSON.stringify(config))
+
+    kumo = new kumoLib(config)
 })
 
 client.on('message', (topic, message) => {
-	logging.info(' ' + topic + ':' + message, {
-		topic: topic,
-		value: message
-	})
-	const components = topic.split('/')
-	const room = components[components.length - 3]
-	const address = kumo.getAddress(room)
+    logging.info(' ' + topic + ':' + message, {
+        topic: topic,
+        value: message
+    })
+    const components = topic.split('/')
+    const room = components[components.length - 3]
+    const address = kumo.getAddress(room)
 
-	if (topic.indexOf('/mode/set') >= 0) {
-		logging.info('setting mode: ' + message + '   room: ' + room)
-		kumo.setMode(address, message.toString())
-	} else if (topic.indexOf('/fanspeed/set') >= 0) {
-		logging.info('setting fan speed: ' + message + '   room: ' + room)
-		kumo.setFanSpeed(address, message.toString())
-	} else if (topic.indexOf('/vanedir/set') >= 0) {
-		logging.info('setting vent direction: ' + message + '   room: ' + room)
-		kumo.setVentDirection(address, message.toString())
-	} else if (topic.indexOf('/cool/set') >= 0) {
-		logging.info('setting cool: ' + message + '   room: ' + room)
-		kumo.setCoolTemp(address, Number(message))
-	} else if (topic.indexOf('/heat/set') >= 0) {
-		logging.info('setting heat: ' + message + '   room: ' + room)
-		kumo.setHeatTemp(address, Number(message))
-	} else {
-		logging.error(' * unhandled command')
-	}
+    if (topic.indexOf('/mode/set') >= 0) {
+        logging.info('setting mode: ' + message + '   room: ' + room)
+        kumo.setMode(address, message.toString())
+    } else if (topic.indexOf('/fanspeed/set') >= 0) {
+        logging.info('setting fan speed: ' + message + '   room: ' + room)
+        kumo.setFanSpeed(address, message.toString())
+    } else if (topic.indexOf('/vanedir/set') >= 0) {
+        logging.info('setting vent direction: ' + message + '   room: ' + room)
+        kumo.setVentDirection(address, message.toString())
+    } else if (topic.indexOf('/cool/set') >= 0) {
+        logging.info('setting cool: ' + message + '   room: ' + room)
+        kumo.setCoolTemp(address, Number(message))
+    } else if (topic.indexOf('/heat/set') >= 0) {
+        logging.info('setting heat: ' + message + '   room: ' + room)
+        kumo.setHeatTemp(address, Number(message))
+    } else {
+        logging.error(' * unhandled command')
+    }
 })
 
 
 const queryKumo = function() {
-	if ( _.isNil(kumo) ) {
-		return 
-	}
+    if (_.isNil(kumo)) {
+        return
+    }
 
-	kumo.getRoomList().forEach(roomLabel => {
-		const address = kumo.getAddress(roomLabel)
-        
-		kumo.getStatus(address).then((result) => {
-			const status = result.r.indoorUnit.status
-			console.log('Room: ' + roomLabel)
-			console.log('   Address: ' + address)
-			console.log('   Status: ' + JSON.stringify(status))
-			Object.keys(status).forEach(key => {
-				const topic = mqtt_helpers.generateTopic(topic_prefix, roomLabel, key)
-				client.smartPublish(topic, status[key].toString(), mqttOptions)
-			})
+    kumo.getRoomList().forEach(roomLabel => {
+        const address = kumo.getAddress(roomLabel)
 
-		})
-    
-	})
+        kumo.getStatus(address).then((result) => {
+            if (_.isNil(result.r) || _.isNil(result.r.indoorUnit) || _.isNil(result.r.indoorUnit.status)) {
+                logging.error('bad result body: ' + JSON.stringify(result))
+                return
+            }
+
+            const status = result.r.indoorUnit.status
+            logging.info('Room: ' + roomLabel)
+            logging.info('   Address: ' + address)
+            logging.info('   Status: ' + JSON.stringify(status))
+
+            Object.keys(status).forEach(key => {
+                const topic = mqtt_helpers.generateTopic(topic_prefix, roomLabel, key)
+                client.smartPublish(topic, status[key].toString(), mqttOptions)
+            })
+
+        })
+
+    })
 
 }
 
 const startPoll = function() {
-	logging.info('Starting to Kumo: ' + username)
-	repeat(queryKumo).every(queryInterval, 's').start.in(1, 'sec')
+    logging.info('Starting to Kumo: ' + username)
+    repeat(queryKumo).every(queryInterval, 's').start.in(1, 'sec')
 }
 
 startPoll()
--- kumojs/kumojs.js
+++ kumojs/kumojs.js
@@ -1,201 +1,202 @@
 'use strict'
 var __awaiter = (this && this.__awaiter) || function(thisArg, _arguments, P, generator) {
-	return new(P || (P = Promise))(function(resolve, reject) {
-		function fulfilled(value) {
-			try {
-				step(generator.next(value))
-			} catch (e) {
-				reject(e)
-			}
-		}
+    return new(P || (P = Promise))(function(resolve, reject) {
+        function fulfilled(value) {
+            try {
+                step(generator.next(value))
+            } catch (e) {
+                reject(e)
+            }
+        }
 
-		function rejected(value) {
-			try {
-				step(generator['throw'](value))
-			} catch (e) {
-				reject(e)
-			}
-		}
+        function rejected(value) {
+            try {
+                step(generator['throw'](value))
+            } catch (e) {
+                reject(e)
+            }
+        }
 
-		function step(result) {
-			result.done ? resolve(result.value) : new P(function(resolve) {
-				resolve(result.value)
-			}).then(fulfilled, rejected)
-		}
-		step((generator = generator.apply(thisArg, _arguments || [])).next())
-	})
+        function step(result) {
+            result.done ? resolve(result.value) : new P(function(resolve) {
+                resolve(result.value)
+            }).then(fulfilled, rejected)
+        }
+        step((generator = generator.apply(thisArg, _arguments || [])).next())
+    })
 }
-Object.defineProperty(exports, '__esModule', {value: true})
+Object.defineProperty(exports, '__esModule', { value: true })
 const sjcl = require('sjcl')
 const request = require('request')
 class Kumo {
-	constructor(config) {
-		this.cfg = config
-		this.cfgr = {}
-		this.cfga = {}
-		for (let acc in config) {
-			for (let hash in config[acc]) {
-				let e = config[acc][hash]
-				this.cfgr[e.label] = e
-				this.cfga[e.address] = e
-			}
-		}
-	}
-	h2l(dt) {
-		var r = []
-		for (var i = 0; i < dt.length; i += 2) {
-			r.push(parseInt(dt.substr(i, 2), 16))
-		}
-		return r
-	}
-	l2h(l) {
-		var r = ''
-		for (var i = 0; i < l.length; ++i) {
-			var c = l[i]
-			if (c < 16) {
-				r += '0'
-			}
-			r += Number(c).toString(16)
-		}
-		return r
-	}
-	getRoomList() {
-		return Object.keys(this.cfgr)
-	}
-	getAddressList() {
-		return Object.keys(this.cfga)
-	}
-	getAddress(label) {
-		if (label in this.cfgr) {
-			return this.cfgr[label].address
-		} else {
-			throw Error('Room ' + label + ' Not Found.')
-		}
-	}
-	cryptokeyFromAddress(dt, address) {
-		let cfg = this.cfga[address]
-		let W = this.h2l(cfg.W)
-		let dt1 = sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(sjcl.codec.hex.toBits(this.l2h(Array.prototype.map.call(dt, function(m2) {
-			return m2.charCodeAt(0)
-		})))))
-		let dt1_l = this.h2l(dt1)
-		let dt2 = ''
-		for (let i = 0; i < 88; i++) {
-			dt2 += '00'
-		}
-		let dt3 = this.h2l(dt2)
-		dt3[64] = 8
-		dt3[65] = 64
-		Array.prototype.splice.apply(dt3, [32, 32].concat(dt1_l))
-		dt3[66] = cfg.S
-		let cryptoserial = this.h2l(cfg['cryptoSerial'])
-		dt3[79] = cryptoserial[8]
-		dt3[80] = cryptoserial[4]
-		dt3[81] = cryptoserial[5]
-		dt3[82] = cryptoserial[6]
-		dt3[83] = cryptoserial[7]
-		dt3[84] = cryptoserial[0]
-		dt3[85] = cryptoserial[1]
-		dt3[86] = cryptoserial[2]
-		dt3[87] = cryptoserial[3]
-		Array.prototype.splice.apply(dt3, [0, 32].concat(W))
-		let hash = sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(sjcl.codec.hex.toBits(this.l2h(dt3))))
-		return hash
-	}
-	cmd_(address, pdata) {
-		return __awaiter(this, void 0, void 0, function*() {
-			return new Promise((resolve, reject) => {
-				let url = 'http://' + address + '/api?m=' + this.cryptokeyFromAddress(pdata, address)
-				request.put({
-					headers: {
-						'Accept': 'application/json, text/plain, */*',
-						'Content-Type': 'application/json'
-					},
-					url: url,
-					body: pdata
-				}, function(error, resp, body) {
-					if (error != null) {
-						console.error('Unable to put data')
-						console.error(error)
-						reject(error)
-					}
-					try {
-						var dt = JSON.parse(body)
-						console.error('body: ' + body)
-						resolve(dt)
-					} catch (e) {
-						console.error('Unable to parse result after  put :', body)
-						console.error(e)
-						reject(e)
-					}
-				})
-			})
-		})
-	}
-	timeout(ms) {
-		return new Promise(resolve => setTimeout(resolve, ms))
-	}
-	cmd(address, pdata) {
-		return __awaiter(this, void 0, void 0, function*() {
-			for (let cnt = 0; cnt < 10; ++cnt) {
-				try {
-					const dt = yield this.cmd_(address, pdata)
-					return dt
-				} catch (e) {
-					console.error('Error when trying to send command ', pdata, ' to ', address)
-					console.error(e)
-					yield this.timeout(5000)
-				}
-			}
-			const dt = yield this.cmd_(address, pdata)
-			return dt
-		})
-	}
-	getStatus(address) {
-		return __awaiter(this, void 0, void 0, function*() {
-			let c = JSON.stringify({'c': {'indoorUnit': {'status': {}}}})
-			return this.cmd(address, c)
-		})
-	}
-	setMode(address, mode) {
-		return __awaiter(this, void 0, void 0, function*() {
-			if (['off', 'heat', 'cool', 'dry', 'vent', 'auto'].indexOf(mode) < 0) {
-				throw new Error('Invalid Mode:' + mode)
-			}
-			let c = JSON.stringify({'c': {'indoorUnit': {'status': {'mode': mode}}}})
-			return this.cmd(address, c)
-		})
-	}
-	setFanSpeed(address, speed) {
-		return __awaiter(this, void 0, void 0, function*() {
-			if (['quiet', 'low', 'powerful', 'auto', 'superPowerful'].indexOf(speed) < 0) {
-				throw new TypeError('Invalid Fan Speed:' + speed)
-			}
-			let c = JSON.stringify({'c': {'indoorUnit': {'status': {'fanSpeed': speed}}}})
-			return this.cmd(address, c)
-		})
-	}
-	setVentDirection(address, dir) {
-		return __awaiter(this, void 0, void 0, function*() {
-			if (['auto', 'horizontal', 'midhorizontal', 'midpoint', 'midvertical',
-				'vertical', 'swing'].indexOf(dir) < 0) {
-				throw new TypeError('Invalid Direction:' + dir)
-			}
-			let c = JSON.stringify({'c': {'indoorUnit': {'status': {'vaneDir': dir}}}})
-			return this.cmd(address, c)
-		})
-	}
-	setCoolTemp(address, tempC) {
-		return __awaiter(this, void 0, void 0, function*() {
-			let c = JSON.stringify({'c': {'indoorUnit': {'status': {'spCool': tempC}}}})
-			return this.cmd(address, c)
-		})
-	}
-	setHeatTemp(address, tempC) {
-		return __awaiter(this, void 0, void 0, function*() {
-			let c = JSON.stringify({'c': {'indoorUnit': {'status': {'spHeat': tempC}}}})
-			return this.cmd(address, c)
-		})
-	}
+    constructor(config) {
+        this.cfg = config
+        this.cfgr = {}
+        this.cfga = {}
+        for (let acc in config) {
+            for (let hash in config[acc]) {
+                let e = config[acc][hash]
+                this.cfgr[e.label] = e
+                this.cfga[e.address] = e
+            }
+        }
+    }
+    h2l(dt) {
+        var r = []
+        for (var i = 0; i < dt.length; i += 2) {
+            r.push(parseInt(dt.substr(i, 2), 16))
+        }
+        return r
+    }
+    l2h(l) {
+        var r = ''
+        for (var i = 0; i < l.length; ++i) {
+            var c = l[i]
+            if (c < 16) {
+                r += '0'
+            }
+            r += Number(c).toString(16)
+        }
+        return r
+    }
+    getRoomList() {
+        return Object.keys(this.cfgr)
+    }
+    getAddressList() {
+        return Object.keys(this.cfga)
+    }
+    getAddress(label) {
+        if (label in this.cfgr) {
+            return this.cfgr[label].address
+        } else {
+            throw Error('Room ' + label + ' Not Found.')
+        }
+    }
+    cryptokeyFromAddress(dt, address) {
+        let cfg = this.cfga[address]
+        let W = this.h2l(cfg.W)
+        let dt1 = sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(sjcl.codec.hex.toBits(this.l2h(Array.prototype.map.call(dt, function(m2) {
+            return m2.charCodeAt(0)
+        })))))
+        let dt1_l = this.h2l(dt1)
+        let dt2 = ''
+        for (let i = 0; i < 88; i++) {
+            dt2 += '00'
+        }
+        let dt3 = this.h2l(dt2)
+        dt3[64] = 8
+        dt3[65] = 64
+        Array.prototype.splice.apply(dt3, [32, 32].concat(dt1_l))
+        dt3[66] = cfg.S
+        let cryptoserial = this.h2l(cfg['cryptoSerial'])
+        dt3[79] = cryptoserial[8]
+        dt3[80] = cryptoserial[4]
+        dt3[81] = cryptoserial[5]
+        dt3[82] = cryptoserial[6]
+        dt3[83] = cryptoserial[7]
+        dt3[84] = cryptoserial[0]
+        dt3[85] = cryptoserial[1]
+        dt3[86] = cryptoserial[2]
+        dt3[87] = cryptoserial[3]
+        Array.prototype.splice.apply(dt3, [0, 32].concat(W))
+        let hash = sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(sjcl.codec.hex.toBits(this.l2h(dt3))))
+        return hash
+    }
+    cmd_(address, pdata) {
+        return __awaiter(this, void 0, void 0, function*() {
+            return new Promise((resolve, reject) => {
+                let url = 'http://' + address + '/api?m=' + this.cryptokeyFromAddress(pdata, address)
+                request.put({
+                    headers: {
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    },
+                    url: url,
+                    body: pdata
+                }, function(error, resp, body) {
+                    if (error != null) {
+                        console.error('Unable to put data')
+                        console.error(error)
+                        reject(error)
+                    }
+                    try {
+                        var dt = JSON.parse(body)
+                        console.error('body: ' + body)
+                        resolve(dt)
+                    } catch (e) {
+                        console.error('Unable to parse result after  put :', body)
+                        console.error(e)
+                        reject(e)
+                    }
+                })
+            })
+        })
+    }
+    timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+    cmd(address, pdata) {
+        return __awaiter(this, void 0, void 0, function*() {
+            for (let cnt = 0; cnt < 10; ++cnt) {
+                try {
+                    const dt = yield this.cmd_(address, pdata)
+                    return dt
+                } catch (e) {
+                    console.error('Error when trying to send command ', pdata, ' to ', address)
+                    console.error(e)
+                    yield this.timeout(5000)
+                }
+            }
+            const dt = yield this.cmd_(address, pdata)
+            return dt
+        })
+    }
+    getStatus(address) {
+        return __awaiter(this, void 0, void 0, function*() {
+            let c = JSON.stringify({ 'c': { 'indoorUnit': { 'status': {} } } })
+            return this.cmd(address, c)
+        })
+    }
+    setMode(address, mode) {
+        return __awaiter(this, void 0, void 0, function*() {
+            if (['off', 'heat', 'cool', 'dry', 'vent', 'auto'].indexOf(mode) < 0) {
+                throw new Error('Invalid Mode:' + mode)
+            }
+            let c = JSON.stringify({ 'c': { 'indoorUnit': { 'status': { 'mode': mode } } } })
+            return this.cmd(address, c)
+        })
+    }
+    setFanSpeed(address, speed) {
+        return __awaiter(this, void 0, void 0, function*() {
+            if (['quiet', 'low', 'powerful', 'auto', 'superPowerful'].indexOf(speed) < 0) {
+                throw new TypeError('Invalid Fan Speed:' + speed)
+            }
+            let c = JSON.stringify({ 'c': { 'indoorUnit': { 'status': { 'fanSpeed': speed } } } })
+            return this.cmd(address, c)
+        })
+    }
+    setVentDirection(address, dir) {
+        return __awaiter(this, void 0, void 0, function*() {
+            if (['auto', 'horizontal', 'midhorizontal', 'midpoint', 'midvertical',
+                    'vertical', 'swing'
+                ].indexOf(dir) < 0) {
+                throw new TypeError('Invalid Direction:' + dir)
+            }
+            let c = JSON.stringify({ 'c': { 'indoorUnit': { 'status': { 'vaneDir': dir } } } })
+            return this.cmd(address, c)
+        })
+    }
+    setCoolTemp(address, tempC) {
+        return __awaiter(this, void 0, void 0, function*() {
+            let c = JSON.stringify({ 'c': { 'indoorUnit': { 'status': { 'spCool': tempC } } } })
+            return this.cmd(address, c)
+        })
+    }
+    setHeatTemp(address, tempC) {
+        return __awaiter(this, void 0, void 0, function*() {
+            let c = JSON.stringify({ 'c': { 'indoorUnit': { 'status': { 'spHeat': tempC } } } })
+            return this.cmd(address, c)
+        })
+    }
 }
 exports.Kumo = Kumo
